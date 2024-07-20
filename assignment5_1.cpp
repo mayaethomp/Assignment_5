6d3511c74e8ef0a7b1cb673c89fa1d8e5f6b617d
Maya Thompson <mayaethomp@gmail.com>
2024-07-20 01:33:25 -0700

    lotto

---
+++ assignment5_1.cpp
@@ -0,0 +1,29 @@
+
+// Maya Thompson
+// CS 1, Section #0107
+// Assignment #5, Problem #1
+// Summary: This program allows the user to play a lotto game by allowing the user to select lotto numbers, and comparing them to randomized computer-generated lotto numbers and 
+
+#include <iostream>
+#include <stdlib.h> 
+#include <iomanip>
+#include <cmath>
+#include <ctime> 
+
+using namespace std;
+
+// FUNCTION PROTOTYPES //
+
+
+
+int main() {
+
+
+
+    return 0;
+}
+
+
+
+// FUNCTION DEFINITIONS //
+
