4e33e0485834f5de2cb9a39e101f7753213492b0
Maya Thompson <mayaethomp@gmail.com>
2024-07-21 23:51:28 -0700

    problem 2

--- assignment5_2.cpp
+++ assignment5_2.cpp
@@ -8,42 +8,65 @@
 
 using namespace std;
 
-
+// declare and initialize variables
+int 
 
 
 // Arrays
 const int ROWS = 15;
 const int SEATS = 20;
 
-int Theater[ROWS][SEATS];
-
-
+char Theater[ROWS][SEATS];
 
 // Function Prototypes //
-void displaySeats(char Array, int, int);
+void showSeats();
 
 
 int main() {
 
+
+
 // Initialize array to all available
-for (int r = 0; r < ROWS; r++) {
-    for (int s = 0; s < SEATS; s++) {
-        Theater[r][s] == '*';
+    for (int r = 0; r < ROWS; r++) {
+        for (int s = 0; s < SEATS; s++) {
+            Theater[r][s] = '*';
+        }
     }
 
 // Display Theatre availability
-displaySeats(Theater, ROWS, SEATS);
+    showSeats();
 
-}
+// Prompt user to choose a menu selection
+    cout << "Menu: " << endl
+         << "1) Buy Ticket" << endl
+         << "2) Total sell and exit" << endl << endl
+         << "Enter your choice";
+    cin >> menuChoice;
 
     return 0;
 }
 
 
-// Function Definitions
-void displaySeats(int Array, int ROWS, int SEATS) {
 
+
+
+
+// Function Definitions
+void showSeats() {
+
+    cout << "* Seats available" << endl
+         << "# Reserved Seats" << endl
+         << "Seats: 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16  17 18 19" << endl;
+    
+    for (int r = 0; r < ROWS; r++) {
+        cout << "Row " << r << " ";
+        for (int s = 0; s < SEATS; s++) {
+            cout << Theater[r][s] << "  ";
+        }
+        cout << endl;
+    }
 }
+
 
 
 
